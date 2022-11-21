5bb6cdc279bd926f05eb2cd9419ea9104edab4e0
Alantis Green <Alantisgreen77@gmail.com>
2022-11-21 16:00:08 -0500

    Alantis's individual part

--- Group/src/MainClass.java
+++ Group/src/MainClass.java
@@ -7,8 +7,7 @@ public class MainClass {
     public static int level;
     public static void main(String[] args) {
         Menu menu = new Menu();
-        //Tennis game = new Tennis(level);
-        //game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 
 }
--- Maahla-2/src/Menu.java
+++ Maahla-2/src/Menu.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Menu extends JFrame {
     private JButton bEasy;
@@ -20,9 +18,7 @@ public class Menu extends JFrame {
 //        f.setSize(400,400);
 //        f.setLayout(null);
 //        f.setVisible(true);
-           bEasy.addActionListener(e -> buttonAction(0));
-
-
+//        bEasy.addActionListener(click);
 
         bMedium =new JButton("Medium");
         panel.add(bMedium);
@@ -32,7 +28,7 @@ public class Menu extends JFrame {
 //        f.setLayout(null);
 //        f.setVisible(true);
 //        bMedium.addActionListener(click);
-        bMedium.addActionListener(e -> buttonAction(1));
+
         bHard =new JButton("Hard");
         panel.add(bHard);
         panel.add(Box.createRigidArea(new Dimension(10, 0)));
@@ -41,20 +37,8 @@ public class Menu extends JFrame {
 //        f.setLayout(null);
 //        f.setVisible(true);
 //        bHard.addActionListener(click);
-        bHard.addActionListener(e -> buttonAction(2));
         this.add(panel);
         this.pack();
         this.setVisible(true);
     }
-    public void buttonAction(int level){
-        System.out.println("A button was pressed");
-        MainClass.level = level;
-        setVisible(false);
-        dispose();
-        Tennis game = new Tennis(level);
-        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-    }
 }
-
